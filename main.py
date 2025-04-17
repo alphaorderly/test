378afcc67006cdaa74b3cf759e3454a76325b9c1
mjson <kazama8928@gmail.com>
2025-04-17 23:18:38 +0900

    test
    sonTest

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def main():
     print("Hello, World! And welcome to the Python world.")
     print("This is an additional message for the user.")
+    print("sonTest")
+    print("sonTest")
+    print("sonTest")
+    print("sonTest")
     print("Goodbye!")
+    print("sonTest")
+    print("sonTest")
 
 if __name__ == "__main__":
     main()
